7acc10cd934c552dc86b6bc6106bfb4361fd7185
antonelladsg <jahn.antonella@gmail.com>
2025-04-21 17:39:26 -0300

    first commit

---
+++ desafios/index.js
@@ -0,0 +1,11 @@
+alert("Cantidad deseada")
+console.log ("holi")
+const nombre="Antonella"
+const apellido="Da silva"
+const nombreCompleto= nombre + " " + apellido
+console.log(nombreCompleto)
+let edad= 32
+console.log ("mi edad es:" + edad)
+let CantidadDeseada= promt("Cantidad deseada")
+let CantidadDeseada = CantidadDeseada x 1200
+alert("Cantidad deseada:" +   CantidadDeseada)
