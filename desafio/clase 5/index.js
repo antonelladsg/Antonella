f42ebc178d1d6083f7a1cbfc05ef9ff3c8579fb0
antonelladsg <jahn.antonella@gmail.com>
2025-05-12 19:47:16 -0300

    Actualizacion

--- desafio/clase 5/index.js
+++ desafio/clase 5/index.js
@@ -1,30 +1,11 @@
-let contador = 0
-while (contador <=3) {
-   console.log ("hola")
-   contador = contador + 1
-    
+let productos =  ["Jumper","Hoodie","Cardigan","Sweater"]
+for(let i=0; i< productos.length ; i++) {
+   console.log(productos[i])
 }
 
-const usuario ="";
-const contraseña = "";
-let intentos = 0;
-const maxIntentos = 3;
-let accesoContenido = false;
-do{
-const pedirUsuario = prompt ("Ingrese su usuario");
-const pedirContraseña = prompt ("Ingrese su contraseña");
-} while (a);
+let productoEliminado = productos.pop();
+console.log(productoEliminado + "sin stock.")
 
-if (contraseña == pedirContraseña && usuario == pedirUsuario) {
-    let accesoContenido = true;
-    alert = "Bienvenido"
-
-} else {
-    intentos++; 
-    if (intentos < maxIntentos)
-       alert ("Incorrecto, intentos restantes: " + (maxIntentos - intentos) )
-
-    } while (intentos < maxIntentos && !accesoContenido);
-
-
+for(let i=0; i< productos.length; i++)
+console.log("Productos en stock:" + productos[i])
 
