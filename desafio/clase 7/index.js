995464f216471a9131801d139c6415e51564e450
antonelladsg <jahn.antonella@gmail.com>
2025-06-02 18:46:00 -0300

    Primer commit: Subir proyecto

---
+++ desafio/clase 7/index.js
@@ -0,0 +1,5 @@
+const precioElemento = document.getElementById("precioProducto");
+        precioElemento.innerText = "$450";
+
+const tituloElemento = document.querySelector("#nombreProducto");
+        tituloElemento.style.color = "pink";
